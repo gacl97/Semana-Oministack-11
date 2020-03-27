cb7159345609bc470eaaedb4b6a3c71e42e55a41
Gustavo <gustavolopes26@gmail.com>
2020-03-27 00:00:26 -0300

    att

--- backend/src/database/connection.js
+++
@@ -1,6 +0,0 @@
-const knex = require('knex');
-const configuration = require('../../knexfile'); // Configuração do banco de dados
-
-const connection = knex(configuration.development); // Pàssando a configuração de desenvolvimento
-
-module.exports = connection;
